b0a1dd2c099c1395f435726c8f6a260047375195
Namhyuk Ben Koh <31713416+nbkoh@users.noreply.github.com>
2017-12-20 16:51:40 -0500

    Update MastersStudent.cpp

--- MastersStudent.cpp
+++ MastersStudent.cpp
@@ -1,4 +1,6 @@
 #include "MastersStudent.h"
+#include <iostream>
+
 
 // Constructor Implementations
 MastersStudent::MastersStudent(Student ug, int msy):
@@ -11,15 +13,32 @@ MastersStudent::MastersStudent(std::string fn, std::string ln,
 // New methods specific to MastersStudent
 float MastersStudent::getMsGpa() {
   // TODO Implement this 
-  return 0.0;
+	float sum = 0;
+
+	int size = _ms_grades.size();
+
+	for(int i = 0; i< size; i++)
+	{
+		sum = _ms_grades[i] + sum;
+	}
+
+	float average = 0;
+
+	average = sum / size;
+
+  return average;
 }
 
 void MastersStudent::addMsGrade(float grade) {
   // TODO Implement this 
+	_ms_grades.push_back(grade);
+	return;
 }
 
 // Override Student's printInfo to include new fields
 void MastersStudent::printInfo() {
   Student::printInfo();
   // TODO Print additional MastersStudent fields
+  printf("MS %s: %d\n", majorString(_major).c_str(), _ms_grad_year);
+  printf("MS GPA: %.2f\n", getMsGpa());
 }
